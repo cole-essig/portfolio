99586b6d6f452cf285129e4696fb5551920d0e35
Cole <hoopwalker@yahoo.com>
2025-06-22 14:46:09 -0700

    initial commit, adding dependencies, added tailwind, updated npm run, added config files, updated vite config

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <>
+      <h1 className="bg-blue-700 text-white text-4xl p-10 font-bold">Tailwind Live!</h1>
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",               // ✔️ root HTML file
+    "./src/**/*.{js,ts,jsx,tsx}"  // ✔️ all files in /src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
