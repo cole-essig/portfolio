004e9984bf642a664cb1166d38dfa5e5f5ae6cb7
Cole <hoopwalker@yahoo.com>
2025-06-23 20:31:38 -0700

    added block folder, filled with initial component list/basic structure

---
+++ src/components/blocks/App/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css"
+ 
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-white text-4xl p-10 font-bold">Tailwind Live!</h1>
+    </div>
+  )
+}
+ 
+export default App
+
